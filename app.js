a328d0aac28659de8b08b1fdd64eb09788c5f9b0
ÖmerÇınar <omercinarr00@gmail.com>
2022-09-28 02:24:45 +0300

    app js add

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ const span = document.querySelector("span")
 const par = document.querySelector("p")
 
 const dice = document.querySelector(".dice")
-
-
-dice.addEventListener("click" , (e)=>{
-    
-})
+dice.addEventListener("click", (e) => {
+   myAdvice();
+});
 
 const myAdvice = async () =>{
     const url = `https://api.adviceslip.com/advice`;
-    
     try {
         const response = await fetch(url);
         if(!response.ok)
@@ -19,6 +16,9 @@ const myAdvice = async () =>{
 
         const data = await response.json();
         console.log(data)
+        console.log(data.slip.id) 
+        span.innerHTML = `${data.slip.id}`;
+        par.innerHTML = `${data.slip.advice}`;
     }
     
     catch (error) {
